b98615a70f8f6f4c98a268fe6aa2896dbd44ddce
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-10-27 14:59:30 -0500

    votes and refactor

--- models/index.js
+++ models/index.js
@@ -1,5 +1,22 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
 
 User.hasMany(Post, {
   foreignKey: 'user_id'
@@ -9,4 +26,16 @@ Post.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
