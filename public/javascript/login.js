929ffb66cfd68ca632e988d9e9f80f596807f7bb
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-11-04 13:19:57 -0500

    breaking for a bit, so I need to commit

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -18,7 +18,7 @@ async function signupFormHandler(event) {
     });
 
     if(response.ok) {
-      console.log('success');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
@@ -42,7 +42,7 @@ async function loginFormHandler(event) {
     });
 
     if (response.ok) {
-      console.log('success');
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
