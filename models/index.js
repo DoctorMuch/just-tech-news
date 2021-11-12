4fc57f44826d38c1d58526de0da57bbb0ed74bcd
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-11-11 22:55:17 -0600

    spending a lot of time with a lot of frustration

--- models/index.js
+++ models/index.js
@@ -4,55 +4,64 @@ const Vote = require('./Vote');
 const Comment = require('./Comment');
 
 Comment.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
 });
 
 Comment.belongsTo(Post, {
   foreign_key: 'post_id',
-  onDelete: 'cascade'
+  onDelete: 'SET NULL'
 });
 
 User.hasMany(Comment, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
 });
 
 Post.hasMany(Comment, {
   foreignKey: 'post_id',
-  onDelete: 'cascade'
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Vote, { 
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
 });
 
 Vote.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
 });
 
 User.hasMany(Vote, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
-Post.hasMany(Vote, { onDelete: 'cascade', hooks:true
-});
-
-Vote.belongsTo(Post, { onDelete: 'cascade'
+Vote.belongsTo(Post, { onDelete: 'SET NULL'
 });
 
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
 
-Post.belongsTo(User, {
-  foreignKey: 'user_id',
-});
-
 User.belongsToMany(Post, {
   through: Vote,
   as: 'voted_posts',
-  foreignKey: 'user_id'
+  foreignKey: 'user_id', 
+  onDelete: 'SET NULL'
 });
 
-Post.belongsToMany(User, {
-  through: Vote,
-  as: 'voted_posts',
-  foreignKey: 'post_id'
-});
+
 
 module.exports = { User, Post, Vote, Comment };
